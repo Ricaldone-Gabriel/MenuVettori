d3fbbb8897496fde6ef39026d27cc1a0c724775e
gabriel.ricaldone <gabriel.ricaldone@InfoTele24.maxwell.local>
2023-01-23 11:44:28 +0100

    modificazione

--- src/progmenuvettori/ProgMenuVettori.java
+++ src/progmenuvettori/ProgMenuVettori.java
@@ -5,6 +5,7 @@
  */
 package progmenuvettori;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -19,24 +20,52 @@ public class ProgMenuVettori {
     public static void main(String[] args) {
         // TODO code application logic here
         boolean running = true;
-        int risposta;
-        Scanner input = new Scanner(System.in);
+        int risposta,i = 0;
+        String rispostaString;
         
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> vettoreStringhe = new ArrayList<String>();
         while(running) {
+            if (!vettoreStringhe.isEmpty()) {
+                i = 0;
+                for( String elemento : vettoreStringhe) {
+                    System.out.println(i + "- " + elemento);
+                    i++;
+                }
+                System.out.println();
+            }
+            
             System.out.println("Cosa vorrebbe fare?");
-            System.out.println("1-");
-            System.out.println("2-");
-            System.out.println("3-");
-            System.out.println("4-");
-            System.out.println("5-");
-            System.out.println("6-");
+            System.out.println("1-Aggiungi elemento");
+            System.out.println("2-Elimina elemento");
+            System.out.println("3-Quantità totale elementi");
+            System.out.println("4-Chiesto un elemento dire dove si trova");
+            System.out.println("5-Chiesta una lettera, visualizzare tutti gli elementi con quella lettera");
+            System.out.println("6-Esci");
+            
             risposta = input.nextInt();
+            input.nextLine();
             switch(risposta) {
                 case 1:
+                    rispostaString = input.nextLine();
+                    vettoreStringhe.add(rispostaString);
                     break;
                 case 2:
+                    if(!vettoreStringhe.isEmpty()) {
+                        risposta = input.nextInt();
+                        input.nextLine();
+                        
+                        if (!(risposta > vettoreStringhe.size()-1)) {
+                            vettoreStringhe.remove(risposta);
+                        } else {
+                            System.out.println("Errore fuori dal vettore");
+                        }
+                    } else {
+                        System.out.println("Il vettore è vuoto");
+                    }
                     break;
                 case 3:
+                    System.out.println("Quantità totale elementi: " + vettoreStringhe.size());
                     break;
                 case 4:
                     break;
